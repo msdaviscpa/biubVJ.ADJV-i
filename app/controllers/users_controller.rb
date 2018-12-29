64d327688405d643073d9785d9045f436173efa6
MSDavis <msdaviscpa@gmail.com>
2018-12-29 04:06:53 +0000

    Add user edit and index functionalities

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,12 @@
 class UsersController < ApplicationController
-
+  
+  def index
+   @users = User.all
+  end
+  
+  def show
+    @user = User.find(params[:id])
+  end
   
   def new
     @user = User.new
@@ -9,6 +16,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)    
     if @user.save
       # Handle a successful save.
+      flash[:success] = "Enrollment Successful"
+      redirect_to @user
     else
       render 'new'
     end
@@ -18,20 +27,23 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
   
- def index
-   @users = User.find(params[:id])
-   
- end
- 
-  def show
-    @user = User.find(params[:id])
-    debugger
+  def update
+    @user = User.find(params[:id])    
+    if @user.update_attributes(user_params)
+      # Handle a successful update.
+      flash[:success] = "Update Successful"
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
+ 
+
   
   private
 
     def user_params
-      params.require(:user).permit(:id, :name, :email, :password,
+      params.require(:user).permit( :name, :email, :password,
                                    :password_confirmation)
     end
   
